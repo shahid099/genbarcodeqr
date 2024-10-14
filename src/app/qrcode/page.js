94baccb4fab757ce4f219a46c3ad7516866a8045
Muhammad Shahid <shahidmuhammadshahid099@gmail.com>
2024-10-14 15:46:32 -0400

    Try to solve problem

--- src/app/qrcode/page.js
+++ src/app/qrcode/page.js
@@ -1,34 +1,9 @@
 "use client"
-import React, { useState } from 'react'
-import { QRCodeSVG } from "qrcode.react"
-
-const page = () => {
-    const [textValue, setTextValue] = useState('');
-    const handleChange = (event)=> {
-        setTextValue(event.target.value);
-    }
-
-
+import Qrcode from "../../../components/Qrcode"
+export default function Qcode() {
   return (
-        <div className='w-full min-h-[40em] mt-5 flex justify-start items-center flex-col gap-3 bg-red-300'>
-                <h1 className='flex text-6xl mt-5'>Generate QRCode</h1>
-            <section className='flex flex-col gap-3'>
-                <input onChange={handleChange} className='px-3 py-2 rounded-md w-[30em]' type='text' placeholder='Enter the text...' />
-                <button className='py-3 bg-orange-300'>Generate</button>
-            </section>
-            <section>
-                <QRCodeSVG 
-                value={textValue}
-                size={128} 
-                bgColor="#ffffff" 
-                fgColor="#00000" 
-                level="H" 
-                marginSize={4} 
-                />
-            </section>
-            <p>{textValue}</p>
+        <div>
+            <Qrcode />
         </div>
-  )
+  );
 }
-
-export default page
