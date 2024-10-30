9e80fa91fc41f3dc644e89bff603cf0a964e275c
Muhammad Shahid <shahidmuhammadshahid099@gmail.com>
2024-10-30 16:47:37 -0400

    resolving Error 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,9 @@
 "use client"
-import { NextSeo } from 'next-seo';
 // Import Components 
 import Barcodegenerate from "../components/Barcodegenerate";
 export default function Home() {
   return (
     <div className="h-full w-[100%]">
-      <>
-        <NextSeo
-        title="Simple Usage Example"
-        description="A short description goes here."
-        />
-      </>
       <main className="h-full flex flex-col">
         <Barcodegenerate />
       </main>
